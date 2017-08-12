9d142218a1435eee22a5b99d58b97d2d3efd5f20
smileygamer6 <rorymcgann@me.com>
2017-08-11 19:31:30 -0600

    Revert "Housekeeping"
    This reverts commit d93c102339f69f99b4cf449b3b2763879fdf8788.

---
+++ Intermediate/Build/Win64/UE4Editor/Inc/MyProject/MyProjectGameMode.generated.h
@@ -0,0 +1,90 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "ObjectMacros.h"
+#include "ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef MYPROJECT_MyProjectGameMode_generated_h
+#error "MyProjectGameMode.generated.h already included, missing '#pragma once' in MyProjectGameMode.h"
+#endif
+#define MYPROJECT_MyProjectGameMode_generated_h
+
+#define Diamond_Diaries_Source_MyProject_MyProjectGameMode_h_12_RPC_WRAPPERS
+#define Diamond_Diaries_Source_MyProject_MyProjectGameMode_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define Diamond_Diaries_Source_MyProject_MyProjectGameMode_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAMyProjectGameMode(); \
+	friend MYPROJECT_API class UClass* Z_Construct_UClass_AMyProjectGameMode(); \
+public: \
+	DECLARE_CLASS(AMyProjectGameMode, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), 0, TEXT("/Script/MyProject"), MYPROJECT_API) \
+	DECLARE_SERIALIZER(AMyProjectGameMode) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define Diamond_Diaries_Source_MyProject_MyProjectGameMode_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesAMyProjectGameMode(); \
+	friend MYPROJECT_API class UClass* Z_Construct_UClass_AMyProjectGameMode(); \
+public: \
+	DECLARE_CLASS(AMyProjectGameMode, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), 0, TEXT("/Script/MyProject"), MYPROJECT_API) \
+	DECLARE_SERIALIZER(AMyProjectGameMode) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define Diamond_Diaries_Source_MyProject_MyProjectGameMode_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	MYPROJECT_API AMyProjectGameMode(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AMyProjectGameMode) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(MYPROJECT_API, AMyProjectGameMode); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AMyProjectGameMode); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	MYPROJECT_API AMyProjectGameMode(AMyProjectGameMode&&); \
+	MYPROJECT_API AMyProjectGameMode(const AMyProjectGameMode&); \
+public:
+
+
+#define Diamond_Diaries_Source_MyProject_MyProjectGameMode_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	MYPROJECT_API AMyProjectGameMode(AMyProjectGameMode&&); \
+	MYPROJECT_API AMyProjectGameMode(const AMyProjectGameMode&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(MYPROJECT_API, AMyProjectGameMode); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AMyProjectGameMode); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AMyProjectGameMode)
+
+
+#define Diamond_Diaries_Source_MyProject_MyProjectGameMode_h_12_PRIVATE_PROPERTY_OFFSET
+#define Diamond_Diaries_Source_MyProject_MyProjectGameMode_h_9_PROLOG
+#define Diamond_Diaries_Source_MyProject_MyProjectGameMode_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Diamond_Diaries_Source_MyProject_MyProjectGameMode_h_12_PRIVATE_PROPERTY_OFFSET \
+	Diamond_Diaries_Source_MyProject_MyProjectGameMode_h_12_RPC_WRAPPERS \
+	Diamond_Diaries_Source_MyProject_MyProjectGameMode_h_12_INCLASS \
+	Diamond_Diaries_Source_MyProject_MyProjectGameMode_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define Diamond_Diaries_Source_MyProject_MyProjectGameMode_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Diamond_Diaries_Source_MyProject_MyProjectGameMode_h_12_PRIVATE_PROPERTY_OFFSET \
+	Diamond_Diaries_Source_MyProject_MyProjectGameMode_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	Diamond_Diaries_Source_MyProject_MyProjectGameMode_h_12_INCLASS_NO_PURE_DECLS \
+	Diamond_Diaries_Source_MyProject_MyProjectGameMode_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID Diamond_Diaries_Source_MyProject_MyProjectGameMode_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
